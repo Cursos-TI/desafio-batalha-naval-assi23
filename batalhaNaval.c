e3ff5fe667fbb9d605060d3b5cd3b5313d29fbb6
Guilherme Assi <guiassi@hotmail.com>
2025-03-22 13:38:24 -0300

    feat: comentarios

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -29,18 +29,22 @@ int main()
 
     printf("     TABULEIRO BATALHA NAVAL \n");
     printf("   ");
+    //Imprime as letras de A a J
     for (int i = 0; i < 10; i++)
     {
         printf("  %c", linha[i]);
     }
 
+    //Imprime as linhas
     for (int i = 0; i < 10; i++)
     {
+        //Imprime os números de 1 a 10
         if (i != 9)
             printf("\n  %d  ", i + 1);
         else
             printf("\n %d  ", i + 1);
 
+        //Imprime os valores da matriz
         for (int j = 0; j < 10; j++)
         {
             printf("%d  ", tabuleiro[i][j]); 
